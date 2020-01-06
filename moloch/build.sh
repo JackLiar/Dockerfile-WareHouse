4b6f3c5009f04de702fecd9264a7679b0cd6db74
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2020-01-06 21:08:46 +0800

    add support for update dependencies before docker build

--- moloch/build.sh
+++ moloch/build.sh
@@ -1,5 +1,25 @@
 # /bin/bash
 
+if [ "$1" == "--update" ];then
+    wget \
+        -O etc/ipv4-address-space.csv \
+        https://www.iana.org/assignments/ipv4-address-space/ipv4-address-space.csv
+
+    wget \
+        -O etc/GeoLite2-Country.mmdb.gz \
+        https://updates.maxmind.com/app/update_secure?edition_id=GeoLite2-Country
+    gunzip -c etc/GeoLite2-Country.mmdb.gz > etc/GeoLite2-Country.mmdb && rm etc/GeoLite2-Country.mmdb.gz
+
+    wget \
+        https://updates.maxmind.com/app/update_secure?edition_id=GeoLite2-ASN \
+        -O etc/GeoLite2-ASN.mmdb.gz
+    gunzip -c etc/GeoLite2-ASN.mmdb.gz > etc/GeoLite2-ASN.mmdb && rm etc/GeoLite2-ASN.mmdb.gz
+
+    wget \
+        -O etc/oui.txt \
+        https://raw.githubusercontent.com/wireshark/wireshark/master/manuf
+fi
+
 export MOLOCH_VERSION=2.1.2
 docker build \
     --build-arg MOLOCH_VERSION=$MOLOCH_VERSION \
