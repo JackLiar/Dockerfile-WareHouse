4a11b60382d2d7e261d9f5ab93d2582b331f0232
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-05-07 19:05:14 +0800

    update rust to 1.52.0; update rust-analyzer to 2021-05-03

--- rust/build.sh
+++ rust/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-export RUST_VERSION=1.51.0
+export RUST_VERSION=1.52.0
 
 docker build \
     --build-arg RUST_VERSION=$RUST_VERSION \
-    --build-arg RUST_ANALYZER_VERSION='2021-03-29' \
-    -t jackwzh/rust:'1.51.0-centos7-devenv' .
+    --build-arg RUST_ANALYZER_VERSION='2021-05-03' \
+    -t jackwzh/rust:"${RUST_VERSION}-centos7-devenv" .
