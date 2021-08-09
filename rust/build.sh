12b8ff4f6ab9dbe6e22ef7514c49f3e2e43af5fb
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2021-08-09 18:33:01 +0800

    udpate rust to 1.54; fix rust centos image system language error

--- rust/build.sh
+++ rust/build.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-export RUST_VERSION=1.52.0
+export RUST_VERSION=1.54.0
 
 docker build \
     --build-arg RUST_VERSION=$RUST_VERSION \
-    --build-arg RUST_ANALYZER_VERSION='2021-05-03' \
-    -t jackwzh/rust:"${RUST_VERSION}-centos7-devenv" .
+    --build-arg RUST_ANALYZER_VERSION='2021-08-09' \
+    -t rust:"${RUST_VERSION}-centos7-devenv" .
