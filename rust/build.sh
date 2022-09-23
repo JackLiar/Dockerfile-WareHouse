b4cf4164a6265b9103a41b4531abb5e800f2f94e
Jack Liar <15205739+JackLiar@users.noreply.github.com>
2022-09-23 23:15:50 +0800

    update rust to 1.64.0

--- rust/build.sh
+++ rust/build.sh
@@ -1,9 +1,7 @@
 #/bin/bash
-export RUST_VERSION=1.59.0
-
+export RUST_VERSION=1.64.0
 
 docker build \
     --build-arg RUST_VERSION=${RUST_VERSION} \
-    --build-arg RUSTUP_VERSION=1.24.3 \
-    --build-arg RUST_ANALYZER_VERSION=2022-02-21 \
+    --build-arg RUSTUP_VERSION=1.25.1 \
     -t rust:"${RUST_VERSION}-centos7-devenv" .
